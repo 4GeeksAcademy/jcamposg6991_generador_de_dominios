7965b33402f1b9cf579b58f733652da063e6a9b8
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-10-09 21:06:24 +0000

    codificacion de generador de dominios en app.js con visualizacion en consola

--- src/app.js
+++ src/app.js
@@ -9,3 +9,22 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+function generarDominios() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".org", ".net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let x = 0; x < noun.length; x++) {
+        for (let y = 0; y < ext.length; y++) {
+          console.log(pronoun[i] + adj[j] + noun[x] + ext[y]);
+        }
+      }
+    }
+  }
+}
+
+generarDominios();
